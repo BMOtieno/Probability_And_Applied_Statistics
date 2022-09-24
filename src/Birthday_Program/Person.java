e601f04284828ac6a05e2e22a226d20aaa593b08
BrianOtieno <48413981+BMOtieno@users.noreply.github.com>
2022-09-24 00:45:40 -0400

    Init

--- src/Birthday_Program/Person.java
+++ src/Birthday_Program/Person.java
@@ -1,22 +1,46 @@
 package Birthday_Program;
 
 import java.math.BigDecimal;
-
+import java.math.BigInteger;
+import java.math.RoundingMode;
 public class Person {
-    public long testDays(int numberOfDaysInAYear){
+    /**
+     * Program that determines the probability that two people share
+     * the same birthday in the class
+     *
+     * @param numberOfDaysInAYear input for the days to be calculated
+     * @return the factorial of the large integer input
+     * @author Brian Otieno Odhiambo
+     */
+    public BigInteger testDays(int numberOfDaysInAYear){
+        BigInteger bigInteger = new BigInteger("1");
 
-        if(numberOfDaysInAYear == 0) return 1;
-        return numberOfDaysInAYear * testDays(numberOfDaysInAYear - 1);
+        for(int i= 2; i <= numberOfDaysInAYear; i++){
+            bigInteger = bigInteger.multiply(BigInteger.valueOf(i));
+        }
+        return bigInteger;
     }
-
+    //method to calculate how many students share the same birthday.
     public BigDecimal sharedBirthday(){
+        int classSize = 30; //number of students in the class
 
-        int classSize = 30;
-        int birthday = 1;
-        long possibleOutcomes = (long) Math.pow(365,classSize);
+        //formula 1 - (365! / (365 - class-size)! x (365 ^ class-size))
+        BigInteger toThePowerClassSize = new BigInteger("365");
+        //365**30
+        BigInteger outCome = toThePowerClassSize.pow(30);
+        //365!
+        BigInteger numerator = testDays(365);
 
-        BigDecimal answer =  (new BigDecimal(testDays(365)).divide(new BigDecimal(testDays(365 - classSize) * possibleOutcomes)));
-        return answer;
+        //the difference between 365 days and class size factorial(335!)
+        BigInteger remainingDays = testDays(335);
+        //(335! * (365**30))
+        BigInteger denominator = remainingDays.multiply(outCome);
 
+        BigDecimal one = new BigDecimal("1");
+        //probability that no one in the class shares the same birthday
+        BigDecimal finalAnswer = new BigDecimal(numerator).divide(new BigDecimal(denominator), 3, RoundingMode.HALF_EVEN);
+
+        //probability that two people in the class share the same birthday
+        return one.subtract(finalAnswer);
     }
 }
--- src/Birthday_Program/PersonTester.java
+++ src/Birthday_Program/PersonTester.java
@@ -3,9 +3,9 @@ package Birthday_Program;
 import org.w3c.dom.ls.LSOutput;
 
 public class PersonTester {
-
     public static void main(String[] args) {
+        //create an instance of the Person class
         Person person = new Person();
-        System.out.println(person.sharedBirthday());
+        System.out.println("The probability that two people in the class are sharing the same birthday is: " + person.sharedBirthday());
     }
 }
